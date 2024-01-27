81f01b927b2b12a62e1d72e2de71b69993752c6f
Tianyi <570703257@qq.com>
2024-01-27 16:45:07 +0800

    Interactable

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { NES } from './jsnes/nes';
 import { Controller } from './jsnes/controller';
 import marioBin from './roms/mario.nes.bin';
 import { Quaternion, Vector3 } from 'babylonjs';
+import { NativeDocument, NativeEngine } from '@yodaos-jsar/dom/src/impl-interfaces';
 
 const { scene } = spatialDocument;
 
@@ -17,7 +18,6 @@ var FRAMEBUFFER_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
 let targetGameObject: BABYLON.Mesh;
 const model = spatialDocument.getNodeById('model');
 
-
 const groundTex = new BABYLON.DynamicTexture("dynamic texture", {
   width: SCREEN_WIDTH,
   height: SCREEN_HEIGHT,
@@ -29,19 +29,14 @@ spatialDocument.addEventListener('spaceReady', () => {
     {
       const mat = new BABYLON.StandardMaterial('mat', scene);
       mat.roughness = 1;
-
       mat.diffuseColor = new BABYLON.Color3(1, 1, 1);
       mat.diffuseTexture = groundTex;
-      
       mat.diffuseTexture.scale(5.4);
-
       screen.material = mat;
-
     }
     targetGameObject = screen as BABYLON.Mesh;
   }
 });
-
 
 const context2d = groundTex.getContext();
 
@@ -70,49 +65,110 @@ scene.registerAfterRender(function () {
   nes.frame();
 });
 
+// floting Gameboy
+// scene.registerAfterRender(() => {
+//   model.getChildMeshes().forEach(child => {
+//     if (child instanceof BABYLON.TransformNode) {
+//       const offsetY = amplitude * Math.sin(t * speed);
+//       child.position.y = offsetY;
+//       child.rotation = new BABYLON.Vector3(offsetY, 0, 0);
+//       }
+//   });
+//   t++;
+// });
 
-scene.registerAfterRender(() => {
-  model.getChildMeshes().forEach(child => {
-    if (child instanceof BABYLON.TransformNode) {
-      const offsetY = amplitude * Math.sin(t * speed);
-      child.position.y = offsetY;
-      child.rotation = new BABYLON.Vector3(offsetY, 0, 0);
-      }
-  });
-  t++;
-});
 
-function keyboard(callback, code) {
-  var player = 1;
-  switch (code) {
-    case 38: // UP
-      callback(player, Controller.BUTTON_UP); break;
-    case 40: // Down
-      callback(player, Controller.BUTTON_DOWN); break;
-    case 37: // Left
-      callback(player, Controller.BUTTON_LEFT); break;
-    case 39: // Right
-      callback(player, Controller.BUTTON_RIGHT); break;
-    case 65: // 'a' - qwerty, dvorak
-    case 81: // 'q' - azerty
-      callback(player, Controller.BUTTON_A); break;
-    case 83: // 's' - qwerty, azerty
-    case 79: // 'o' - dvorak
-      callback(player, Controller.BUTTON_B); break;
-    case 9: // Tab
-      callback(player, Controller.BUTTON_SELECT); break;
-    case 13: // Return
-      callback(player, Controller.BUTTON_START); break;
-    default: break;
+const buttons = spatialDocument.querySelectorAll('ref');
+let buttonA;
+let buttonB;
+let buttonSTART;
+
+for (const button of buttons) {
+  if (button.id === 'model.GB_03_low_ButtonA.001__0.GB_03_low_ButtonA.001__0') {
+    buttonA = button;
+  }
+
+  if (button.id === 'model.GB_03_low_ButtonA__0.GB_03_low_ButtonA__0') {
+    buttonB = button;
+  }
+  
+  if (button.id === 'model.GB_03_low_ButtonSelect__0.GB_03_low_ButtonSelect__0') {
+    buttonSTART = button;
   }
 }
 
-spatialDocument.watchInputEvent();
-spatialDocument.addEventListener('mouse', (event: any) => {
-  const { inputData } = event;
-  if (inputData.Action === 'down') {
-    keyboard(nes.buttonDown, 13);
-  } else if (inputData.Action === 'up') {
-    keyboard(nes.buttonUp, 13);
-  }
+if (buttonA) {
+  buttonA.addEventListener('raydown', () => {
+    console.log('raydown_BUTTON_A');
+    nes.buttonDown(1, Controller.BUTTON_A);
+  });
+  buttonA.addEventListener('rayup', () => {
+    console.log('rayup_BUTTON_A');
+    nes.buttonUp(1, Controller.BUTTON_A);
+  });
+}
+
+if (buttonB) {
+  buttonB.addEventListener('raydown', () => {
+    console.log('raydown_BUTTON_B');
+    nes.buttonDown(1, Controller.BUTTON_B);
+  });
+  buttonB.addEventListener('rayup', () => {
+    console.log('rayup_BUTTON_B');
+    nes.buttonUp(1, Controller.BUTTON_B);
+  });
+}
+
+if (buttonSTART) {
+  buttonSTART.addEventListener('raydown', () => {
+    console.log('raydown_BUTTON_START');
+    nes.buttonDown(1, Controller.BUTTON_START);
+  });
+  buttonSTART.addEventListener('rayup', () => {
+    console.log('rayup_BUTTON_START');
+    nes.buttonUp(1, Controller.BUTTON_START);
+  });
+}
+
+
+const container = spaceDocument.querySelector('#container');
+
+const box_LEFT = spaceDocument.createElement('cube');
+box_LEFT.setAttribute('size', '0.6');
+container.appendChild(box_LEFT);
+var mesh_LEFT = box_LEFT.asNativeType()
+
+const box_RIGHT = spaceDocument.createElement('cube');
+box_RIGHT.setAttribute('size', '0.6');
+container.appendChild(box_RIGHT);
+var mesh_RIGHT = box_RIGHT.asNativeType()
+
+var invisableMaterial = new BABYLON.StandardMaterial("transparentMaterial", scene);
+invisableMaterial.alpha = 0.1;
+
+if (mesh_RIGHT instanceof BABYLON.Mesh) {
+  mesh_RIGHT.position = new BABYLON.Vector3(-1, -0.3, -0.7);
+  mesh_RIGHT.material = invisableMaterial;
+}
+if (mesh_LEFT instanceof BABYLON.Mesh) {
+  mesh_LEFT.position = new BABYLON.Vector3(-1.7, -0.3, -0.7);
+  mesh_LEFT.material = invisableMaterial;
+}
+
+box_LEFT.addEventListener('raydown', () => {
+  console.log('raydown_BUTTON_LEFT');
+  nes.buttonDown(1, Controller.BUTTON_LEFT);
+});
+box_LEFT.addEventListener('rayup', () => {
+  console.log('rayup_BUTTON_LEFT');
+  nes.buttonUp(1, Controller.BUTTON_LEFT);
+});
+
+box_RIGHT.addEventListener('raydown', () => {
+  console.log('raydown_BUTTON_RIGHT');
+  nes.buttonDown(1, Controller.BUTTON_RIGHT);
+});
+box_RIGHT.addEventListener('rayup', () => {
+  console.log('rayup_BUTTON_RIGHT');
+  nes.buttonUp(1, Controller.BUTTON_RIGHT);
 });
